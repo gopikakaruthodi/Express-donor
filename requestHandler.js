5be7daf952e4b306252a985199fa81c789d5526b
gopika <gopikaksurendran@gmail.com>
2024-10-03 13:00:10 +0530

    Added

--- requestHandler.js
+++ requestHandler.js
@@ -2,6 +2,8 @@ import { error } from "console";
 import donorSchema from "./models/donor.model.js"
 import userSchema from "./models/user.model.js"
 import bcrypt from "bcrypt"
+import jwt from "jsonwebtoken"
+const {sign}=jwt
 
 export async function addDonors(req,res){
     try {
@@ -127,5 +129,34 @@ export async function signUp(req,res) {
     
 
            
+    
+}
+signIn
+export async function signIn(req,res) {
+    try {
+        // console.log(req.body);
+    
+        const {email,password}=req.body    
+        if(!(email&&password))
+            return res.status(404).send({msg:"Fields empty"})
+        const user=await userSchema.findOne({email})
+        if(!user)
+            return res.status(404).send({msg:"Invalid Email"})
+        const success=await bcrypt.compare(password,user.password)
+        if(success!=true)
+            return res.status(404).send({msg:"Invalid Password"})
+        const token=await sign({userId:user._id},process.env.JWT_KEY,{expiresIn:"24h"})
+        // console.log(token);
+        res.status(200).send({msg:"Successfully logged in",token})
+        
+
+
+    } catch (error) {
+        console.log(error);
+        
+        
+    }
+    
+
     
 }
--- router.js
+++ router.js
@@ -9,6 +9,7 @@ router.route("/updatedonor/:_id").put(rh.updateDonor)
 router.route("/deletedonor/:_id").delete(rh.deleteDonor)
 // user authentification
 router.route("/signup").post(rh.signUp)
+router.route("/signin").post(rh.signIn)
 
 
 export default router;
